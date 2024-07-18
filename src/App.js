9c4fe7f611d0e03414c1cdf232e758ba3399380e
Kunal Kumbhakar <kumbhakarkunal066@gmail.com>
2024-07-18 23:09:07 +0530

    deploy react to github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import TicTacToe from './Components/TicTacToe/TicTacToe';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+ import './App.css';
+
+function App() {
+ return (
+     <div>
+      <TicTacToe/>
+    </div>
+  );
+ }
+
+ export default App;
+
+
